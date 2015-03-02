30b737eadbb2e356b552591cf20d3fde39c13c5c
vladimir-repin <vladimir.repin@exigenservices.com>
2015-03-02 17:13:45 +0300

    removing unneeded logic

--- MainLogic/Engage/TargettingLogic.cs
+++ MainLogic/Engage/TargettingLogic.cs
@@ -41,51 +41,9 @@
             diff = anglesCalculator.GetHeadingDiff(nextHeading, storage.Robot.GunHeading);
             var neededRate = anglesCalculator.GetHeadingDiff(diff, rates.BodyTurn);
 
-            if (neededRate < 0)
-            {
-                UpdateRatesNegative(neededRate, rates, enemy);
-            }
-            else
-            {
-                UpdateRatesPositive(neededRate, rates, enemy);
-            }
-
-        }
-
-        private void UpdateRatesNegative(double neededRate, Rates rates, EnemyData enemy)
-        {
-            if (neededRate >= -Rules.GUN_TURN_RATE || enemy.Distance < storage.Robot.Height * 1.5)
-            {
-                rates.TurretTurn = Math.Max(neededRate, -Rules.GUN_TURN_RATE);
-                return;
-            }
-
-            var bodyAdjust = neededRate.AddAngle(Rules.GUN_TURN_RATE);
-            if (bodyAdjust < -2)
-            {
-                rates.Velocity = rates.Velocity / 2;
-            }
-
-            rates.BodyTurn = Math.Max(rates.BodyTurn.AddAngle(bodyAdjust), -Rules.MAX_TURN_RATE);
-            rates.TurretTurn = -Rules.GUN_TURN_RATE;
-        }
-
-        private void UpdateRatesPositive(double neededRate, Rates rates, EnemyData enemy)
-        {
-            if (neededRate <= Rules.GUN_TURN_RATE || enemy.Distance < storage.Robot.Height * 1.5)
-            {
-                rates.TurretTurn = Math.Min(neededRate, Rules.GUN_TURN_RATE);
-                return;
-            }
-
-            var bodyAdjust = neededRate.AddAngle(-Rules.GUN_TURN_RATE);
-            if (bodyAdjust > 2)
-            {
-                rates.Velocity = rates.Velocity / 2;
-            }
-
-            rates.BodyTurn = Math.Min(rates.BodyTurn.AddAngle(bodyAdjust), Rules.MAX_TURN_RATE);
-            rates.TurretTurn = Rules.GUN_TURN_RATE;
+            rates.TurretTurn = neededRate < 0
+                                   ? Math.Max(neededRate, -Rules.GUN_TURN_RATE)
+                                   : Math.Min(neededRate, Rules.GUN_TURN_RATE);
         }
     }
 }
